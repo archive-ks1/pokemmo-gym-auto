4c07dc0fe2676c1a345939987e8a621d39367257
Kah Seng <S10208180@connect.np.edu.sg>
2021-08-22 01:49:49 +0800

    Add Cerulean pathing in

--- region/kanto.py
+++ region/kanto.py
@@ -7,7 +7,7 @@ except ModuleNotFoundError:
 
 def menu():
     print("Kanto Gyms")
-    menu_options = ["Pewter", "Cerulean (WIP)", "Vermillion (WIP)", "Celadon (WIP)", "Fuchsia (WIP)", "Saffron (WIP)", "Cinnabar (WIP)", "Viridian (Unavailable)", "Back"]
+    menu_options = ["Pewter", "Cerulean", "Vermillion (WIP)", "Celadon (WIP)", "Fuchsia (WIP)", "Saffron (WIP)", "Cinnabar (WIP)", "Viridian (Unavailable)", "Back"]
 
     for i in range(len(menu_options)):
         print("[{}] {}".format(i+1, menu_options[i]))
@@ -17,11 +17,12 @@ def initialize(gym):
     for sec in range(5, 0, -1):
         sleep(1)
         print("Starting Auto in {} sec(s)...".format(sec))
-
+    
     sleep(1)
+    print("In Progress...")
 
-def pewter_city():
-    initialize("Pewter City")
+def pewter_pathing_in():
+    initialize("Pewter")
     pg.press('2')
     pg.keyDown('d')
     sleep(0.5)
@@ -41,16 +42,31 @@ def pewter_city():
     pg.keyDown('w')
     sleep(0.2)
     pg.keyUp('w')
+    print("Entering Pewter City Gym...")
+
+def cerulean_pathing_in():
+    initialize("Cerulean")
+    pg.press('2')
+    pg.keyDown('s')
+    sleep(0.02)
+    pg.keyUp('s')
+    pg.keyDown('d')
+    sleep(0.62)
+    pg.keyUp('d')
+    pg.keyDown('w')
+    sleep(0.32)
+    pg.keyUp('w')
+    print("Entering Cerulean City Gym...")
 
 def main():
     menu()
     user_input = int(input('\nEnter option: '))
 
     if user_input == 1:
-        pewter_city()
+        pewter_pathing_in()
     
     elif user_input == 2:
-        print("Unavailable")
+        cerulean_pathing_in()
 
     elif user_input == 3:
         print("Unavailable")
